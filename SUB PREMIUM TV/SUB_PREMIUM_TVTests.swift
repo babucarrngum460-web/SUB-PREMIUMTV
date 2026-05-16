35b923bd56bf48f65181ecc52348b36de17721e5
babucarrngum460-web <babucarrngum460@gmail.com>
2026-05-15 22:33:31 -0700

    Add files via upload

---
+++ SUB PREMIUM TV/SUB_PREMIUM_TVTests.swift
@@ -0,0 +1,16 @@
+//
+//  SUB_PREMIUM_TVTests.swift
+//  SUB PREMIUM TVTests
+//
+//  Created by Babucarr Ngum on 5/15/26.
+//
+
+import Testing
+
+struct SUB_PREMIUM_TVTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
